86dfe6245f99fc14d657681732230f889e34a08a
amuga.otito(computer dorm) <urphasin@gmail.com>
2025-09-04 20:49:38 -0600

    im going thru changes

--- script.js
+++ script.js
@@ -10,41 +10,16 @@ const [CANVAS_WIDTH, CANVAS_HEIGHT] = [canvas.width = 600, canvas.height = 600];
 const PlayerImage = new Image();
 PlayerImage.src = "./sprites/shadow_dog.png";
 
-let x = 0;
-let y = 0;
-
-let recWidth = 100;
-let x1 = CANVAS_WIDTH - recWidth; 
-let y1 = 0;
 
 function animate() {
-  if (x > 600) {
-    x = 0;
-  }
-  if (y > 600) {
-    y = 0;
-  }
-
-  if (x1 < -100) {
-    x1 = CANVAS_WIDTH - 100;
-  }
-  if (y1 > 600) {
-    y1 = 0;
-  }
+  
 
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-
-  ctx.fillStyle = "black";
-  ctx.fillRect(x1, y1, 100, 100);
-  
   ctx.fillStyle = "purple";
-  ctx.fillRect(x, y, 100, 100);
+  // ctx.fillRect(100, 100, 100, 100);
 
-  x++;
-  y++;
+  ctx.drawImage(PlayerImage, -1850, -4700,);
 
-  x1--;
-  y1++;
   requestAnimationFrame(animate);
 };
 
